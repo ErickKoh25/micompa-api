e174d1baa4fc4b1a55e3a3877c96e408fa877bd5
Erick Koh <erick.koh@truehome.com.mx>
2022-08-06 18:55:48 -0500

    Allow Cors github

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import config from './config/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['*', 'http://localhost:3000', 'https://erickkoh25.github.io/'],
     methods: ['GET', 'POST', 'PATCH', 'PUT'],
     credentials: true,
   });
